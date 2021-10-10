096d15e76c4c117ba50f60c7bc42ea410fb0079a
jribeiroo <nicolas.jnr10@outlook.com>
2021-10-09 23:50:52 -0300

    first commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+
+import { RoutingModule } from './routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//*pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
